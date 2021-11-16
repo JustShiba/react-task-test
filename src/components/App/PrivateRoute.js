5375a4ec9a32cf8c661ea4c4edccfa7a2adf2f61
IlliaLisitsa <liyalisica9356@gmail.com>
2021-11-16 16:39:20 +0300

    fix let

--- src/components/App/PrivateRoute.js
+++ src/components/App/PrivateRoute.js
@@ -2,8 +2,7 @@ import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
 export function PrivateRoute({ component: Component, ...rest }) {
-    let { auth } = useSelector(state => state.authorization)
-
+    const { auth } = useSelector(state => state.authorization)
 
     return (
         <Route {...rest} render={props => {
